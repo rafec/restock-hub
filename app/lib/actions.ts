d83653e563effc1a23edae57ab38d2ce4ee9faa4
rafec <rafael.brizuena@gmail.com>
2024-01-19 19:51:57 -0400

    instaled `zod` to validate data and created query to insert data into the database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,35 @@
 "use server";
 
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+	id: z.string(),
+	customer_id: z.string(),
+	description: z.string(),
+	keywords: z.string(),
+	date: z.date(),
+	status: z.string(),
+});
+
+const CreateDemand = FormSchema.omit({ id: true, date: true, status: true });
+
 export async function createDemand(formData: FormData) {
-	const rawFormData = {
+	const { customer_id, description, keywords } = CreateDemand.parse({
 		customer_id: formData.get("customer_id"),
 		description: formData.get("description"),
 		keywords: formData.get("keywords"),
-		date: new Date(),
-		status: "open",
-	};
-	console.log(rawFormData);
+	});
+	const date = new Date().toISOString();
+	const status = "open";
+
+	await sql`
+    INSERT INTO demands (customer_id, description, keywords, date, status)
+    VALUES (${customer_id}, ${description}, ${keywords}, ${date}, ${status});
+    `;
+
+	revalidatePath("/dashboard/demands");
+	redirect("/dashboard/demands");
 }
