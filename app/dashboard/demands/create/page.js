be4a59cf9a3a35a2aa6fd836461db7169688226e
rafec <rafael.brizuena@gmail.com>
2024-01-30 19:51:40 -0400

    cleaning working tree to be able to understand better the fundamental concepts of the system

--- app/dashboard/demands/create/page.js
+++
@@ -1,9 +0,0 @@
-import Form from "@/app/ui/demands/create-form";
-
-export default async function Page() {
-	return (
-		<main>
-			<Form />
-		</main>
-	);
-}
--- app/dashboard/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading() {
-	return <h1 className="font-bold text-2xl">Loading...</h1>;
-}
