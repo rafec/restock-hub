87eae28fde62d01b47622c59a8608943cf2f10bc
rafec <rafael.brizuena@gmail.com>
2024-01-29 22:19:56 -0400

    removed authentication with NextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,6 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { signIn } from "@/auth";
-import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
 	id: z.string(),
@@ -34,23 +32,4 @@ export async function createDemand(formData: FormData) {
 
 	revalidatePath("/dashboard/demands");
 	redirect("/dashboard/demands");
-}
-
-export async function authenticate(
-	prevState: string | undefined,
-	formData: FormData
-) {
-	try {
-		await signIn("credentials", formData);
-	} catch (error) {
-		if (error instanceof AuthError) {
-			switch (error.type) {
-				case "CredentialsSignin":
-					return "Invalid credentials.";
-				default:
-					return "Something went wrong.";
-			}
-		}
-		throw error;
-	}
 }
