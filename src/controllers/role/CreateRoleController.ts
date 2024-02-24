11f04e83acb783c3a6436aa70ce6fcdb147f3835
rafec <rafael.brizuena@gmail.com>
2024-02-23 22:22:28 -0400

    wraped `CreateRoleController.handle` method execution in a try/catch and improved a bit the error messages

--- src/controllers/role/CreateRoleController.ts
+++ src/controllers/role/CreateRoleController.ts
@@ -3,12 +3,19 @@ import { CreateRoleService } from "src/services/role/CreateRoleService";
 
 class CreateRoleController {
   async handle(request: Request, response: Response) {
-    const { roleName } = request.body;
-    const createRoleService = new CreateRoleService();
+    try {
+      const { roleName } = request.body;
+      const createRoleService = new CreateRoleService();
 
-    const role = await createRoleService.execute(roleName);
+      const role = await createRoleService.execute(roleName);
 
-    return response.json(role);
+      return response
+        .status(201)
+        .json({ message: "Succesfull operation! Role created.", data: role });
+    } catch (error) {
+      console.log("Error creating role: ", error);
+      response.status(400).json({ message: error.message, data: [] });
+    }
   }
 }
 
