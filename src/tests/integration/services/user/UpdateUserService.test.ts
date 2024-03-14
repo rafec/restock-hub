90fccf40ba4b99627929883ba180280fef21d96e
rafec <rafael.brizuena@gmail.com>
2024-03-14 18:36:12 -0400

    added tests that were missing in `UpdateUserService.test.ts`

--- src/tests/integration/services/user/UpdateUserService.test.ts
+++ src/tests/integration/services/user/UpdateUserService.test.ts
@@ -83,6 +83,48 @@ describe("PUT /user", () => {
     expect(updatedUser.email).toBe(existentUser.email);
   });
 
+  it("Should throw an error when email format is invalid", async () => {
+    const newInvalidEmailUser: IUserRequest = {
+      id: user.id,
+      name: "Invalid email user",
+      email: "invalidmailuser.com",
+      password: "password",
+      roleId: "role-id-123",
+    };
+
+    await expect(
+      updateUserService.execute(newInvalidEmailUser, testPrisma),
+    ).rejects.toThrow("Invalid email address format.");
+  });
+
+  it("Should throw an error when password lenght < 8", async () => {
+    const newInvalidPasswordUser: IUserRequest = {
+      id: user.id,
+      name: "Invalid password user",
+      email: "invalidpassworduser@mail.com",
+      password: "12345",
+      roleId: "role-id-123",
+    };
+
+    await expect(
+      updateUserService.execute(newInvalidPasswordUser, testPrisma),
+    ).rejects.toThrow("Password must be at least 8 characters long.");
+  });
+
+  it("Should throw an error when role doesnt exists", async () => {
+    const newInvalidRoleIdUser: IUserRequest = {
+      id: user.id,
+      name: "Invalid role user",
+      email: "invalidroleuser@mail.com",
+      password: "12345678",
+      roleId: "role-id-123",
+    };
+
+    await expect(
+      updateUserService.execute(newInvalidRoleIdUser, testPrisma),
+    ).rejects.toThrow("Role not found.");
+  });
+
   it("Should throw an error when user already exists", async () => {
     const updateUserProperties: IUserRequest = {
       id: user.id,
