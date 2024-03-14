16e25509d8a549b15ed3d59269b5321446b8e642
rafec <rafael.brizuena@gmail.com>
2024-03-14 18:51:10 -0400

    added tests that were missing in `UpdateProductService.test.ts`

--- src/tests/integration/services/product/UpdateProductService.test.ts
+++ src/tests/integration/services/product/UpdateProductService.test.ts
@@ -71,6 +71,46 @@ describe("PUT /product", () => {
     );
   });
 
+  it("Should throw an error when 3 > product name < 255", async () => {
+    const newInvalidProductNameProduct: IProductRequest = {
+      id: product.id,
+      productName: "Pr",
+      price: 120,
+    };
+
+    await expect(
+      updateProductService.execute(newInvalidProductNameProduct, testPrisma),
+    ).rejects.toThrow(
+      "Product name must be between 3 and 255 characters long.",
+    );
+  });
+
+  it("Should throw an error when price < 0 or is not a number", async () => {
+    const newInvalidPriceProduct: IProductRequest = {
+      id: product.id,
+      productName: "Invalid price product",
+      price: -1,
+    };
+
+    await expect(
+      updateProductService.execute(newInvalidPriceProduct, testPrisma),
+    ).rejects.toThrow("Price must be a positive number.");
+  });
+
+  it("Should throw an error when description > 1000 characters", async () => {
+    const newInvalidDescriptionProduct: IProductRequest = {
+      id: product.id,
+      productName: "Invalid description product",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices vitae auctor eu augue ut lectus arcu. Sed id semper risus in hendrerit gravida. Mi quis hendrerit dolor magna eget. Mattis aliquam faucibus purus in massa tempor nec feugiat. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Metus vulputate eu scelerisque felis. Nec tincidunt praesent semper feugiat nibh. Vel facilisis volutpat est velit egestas dui. Ultrices in iaculis nunc sed augue lacus viverra vitae. Congue mauris rhoncus aenean vel elit. Turpis egestas integer eget aliquet nibh. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec Sed tempus urna et pharetra pharetra. Malesuada bibendum arcu vitae elementum curabitur. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Sit amet venenatis urna cursus eget",
+      price: 120,
+    };
+
+    await expect(
+      updateProductService.execute(newInvalidDescriptionProduct, testPrisma),
+    ).rejects.toThrow("Description must be less than 1000 characters long.");
+  });
+
   it("Should throw an error when product doesnt exists", async () => {
     const updateProductProperties: IProductRequest = {
       id: "invalid-id",
